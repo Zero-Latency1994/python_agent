5751e4190f5d911c3dffcfdeeb9f197d56d57ede
haiyang.zhao <3867326978@qq.com>
2026-04-18 15:48:30 +0800

    测试代码

--- src/my_app/agent.py
+++ src/my_app/agent.py
@@ -1,3 +1,21 @@
+from turtle import mode
 from langchain.agents import create_agent
 import os
 from langchain.tools import tool
+
+
+@tool
+def search(query):
+    """Search for information"""
+    return f"{query}"
+
+
+@tool
+def get_weather(location):
+    """Get weather information for a location"""
+    return f"{location}"
+
+
+agent = create_agent(model="", tools=[search, get_weather])
+
+print(agent)
